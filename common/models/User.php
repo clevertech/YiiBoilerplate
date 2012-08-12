ffcb45bc6ccf6ed8c47b29f17d42e384ec8c27bc
Antonio Ramirez <antonio@clevertech.biz>
2012-08-12 22:04:13 +0200

    added missing migrations for User creation + modified User table

--- common/models/User.php
+++ common/models/User.php
@@ -11,8 +11,6 @@
  *
  * The followings are the available columns in table '{{user}}':
  * @property integer $id
- * @property string $name
- * @property string $surname
  * @property string $password
  * @property string $salt
  * @property string $email
@@ -21,6 +19,8 @@
  * @property integer $login_attempts
  * @property integer $login_time
  * @property string $validation_key
+ * @property string $passwordStrategy
+ * @property boolean $requiresNewPassword
  * @property integer $create_id
  * @property integer $create_time
  * @property integer $update_id
@@ -29,7 +29,8 @@
  * @property integer $delete_time
  * @property integer $status
  */
-class User extends CActiveRecord {
+class User extends CActiveRecord
+{
 
 	/**
 	 * @var string attribute used for new passwords on user's edition
@@ -45,22 +46,26 @@ class User extends CActiveRecord {
 	 * Returns the static model of the specified AR class.
 	 * @return Customer the static model class
 	 */
-	public static function model($className = __CLASS__) {
+	public static function model($className = __CLASS__)
+	{
 		return parent::model($className);
 	}
 
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName() {
-		return '{{user}}';
+	public function tableName()
+	{
+		return 'user';
 	}
 
 	/**
 	 * Behaviors
 	 * @return array
 	 */
-	public function behaviors() {
+	public function behaviors()
+	{
+		Yii::import('common.extensions.behaviors.password.*');
 		return array(
 			// Password behavior strategy
 			"APasswordBehavior" => array(
@@ -84,45 +89,33 @@ class User extends CActiveRecord {
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules() {
+	public function rules()
+	{
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('name, surname, email', 'required', 'on' => 'checkout'),
+			array('email', 'required', 'on' => 'checkout'),
 			array('email', 'unique', 'on' => 'checkout', 'message' => Yii::t('validation', 'Email has already been taken.')),
 			array('email', 'email'),
 			array('username, email', 'unique'),
 			array('passwordConfirm', 'compare', 'compareAttribute' => 'newPassword', 'message' => Yii::t('validation', "Passwords don't match")),
-			array('newPassword, password_strategy', 'length', 'max' => 50, 'min' => 8),
-			array('name, surname, password', 'length', 'max' => 45),
-			array('email, password, salt', 'length', 'max' => 125),
-			array('requires_new_password, login_attempts', 'numerical', 'integerOnly'=> true),
+			array('newPassword, passwordStrategy ', 'length', 'max' => 50, 'min' => 8),
+			array('email, password, salt', 'length', 'max' => 255),
+			array('requiresNewPassword, login_attempts', 'numerical', 'integerOnly' => true),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, name, surname, password, salt, password_strategy, requires_new_password, email', 'safe', 'on' => 'search'),
-		);
-	}
-
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations() {
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			//'creditCard' => array(self::HAS_MANY, 'BillingInfo', 'user_id'),
+			array('id, password, salt, passwordStrategy , requiresNewPassword , email', 'safe', 'on' => 'search'),
 		);
 	}
 
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels() {
+	public function attributeLabels()
+	{
 		return array(
 			'id' => 'ID',
 			'username' => Yii::t('labels', 'Username'),
-			'name' => Yii::t('labels', 'Name'),
-			'surname' => Yii::t('labels', 'Surname'),
 			'password' => Yii::t('labels', 'Password'),
 			'newPassword' => Yii::t('labels', 'Password'),
 			'passwordConfirm' => Yii::t('labels', 'Confirm password'),
@@ -134,16 +127,18 @@ class User extends CActiveRecord {
 	 * Helper property function
 	 * @return string the full name of the customer
 	 */
-	public function getFullName() {
+	public function getFullName()
+	{
 
-		return ucwords($this->name . ' ' . $this->surname);
+		return $this->username;
 	}
 
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
-	public function search() {
+	public function search()
+	{
 		// Warning: Please modify the following code to remove attributes that
 		// should not be searched.
 
@@ -151,8 +146,6 @@ class User extends CActiveRecord {
 
 		$criteria->compare('id', $this->id);
 		$criteria->compare('username', $this->username, true);
-		$criteria->compare('name', $this->name, true);
-		$criteria->compare('surname', $this->surname, true);
 		$criteria->compare('password', $this->password, true);
 		$criteria->compare('email', $this->email, true);
 
@@ -166,7 +159,8 @@ class User extends CActiveRecord {
 	 * (emails by standard can have uppercase letters)
 	 * @return parent::beforeValidate
 	 */
-	public function beforeValidate() {
+	public function beforeValidate()
+	{
 		if (!empty($this->username))
 			$this->username = strtolower($this->username);
 		return parent::beforeValidate();
@@ -175,7 +169,8 @@ class User extends CActiveRecord {
 	/**
 	 * Generates a new validation key (additional security for cookie)
 	 */
-	public function regenerateValidationKey() {
+	public function regenerateValidationKey()
+	{
 		$this->saveAttributes(array(
 			'validation_key' => md5(mt_rand() . mt_rand() . mt_rand()),
 		));
---
+++ console/migrations/m120805_131754_user_table_migration.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * m120805_131754_user_table_migration.php
+ *
+ * @author: antonio ramirez <antonio@clevertech.biz>
+ * Date: 8/12/12
+ * Time: 9:52 PM
+ */
+class m120805_131754_user_table_migration extends CDbMigration
+{
+	public function up()
+	{
+		$this->execute('CREATE TABLE `user` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`username` varchar(45) DEFAULT NULL,
+			`password` varchar(255) DEFAULT NULL,
+			`salt` varchar(255) DEFAULT NULL,
+			`passwordStrategy` varchar(50) DEFAULT NULL,
+			`requiresNewPassword` tinyint(1) DEFAULT NULL,
+			`email` varchar(255) DEFAULT NULL,
+			`login_attempts` int(11) DEFAULT NULL,
+			`login_time` int(11) DEFAULT NULL,
+			`login_ip` varchar(32) DEFAULT NULL,
+			`validation_key` varchar(255) DEFAULT NULL,
+			`create_id` int(11) DEFAULT NULL,
+			`create_time` int(11) DEFAULT NULL,
+			`update_id` int(11) DEFAULT NULL,
+			`update_time` int(11) DEFAULT NULL,
+			PRIMARY KEY (`id`),
+			UNIQUE KEY `username` (`username`),
+			UNIQUE KEY `email` (`email`)
+		) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8');
+
+		/* add demo users */
+		$demoUser = new User();
+		$demoUser->username = "demo";
+		$demoUser->email = "demo@clevertech.biz";
+		$demoUser->password = "clevertech";
+
+		$demoUser->save();
+
+		$adminUser = new User();
+		$adminUser->username = "admin";
+		$adminUser->email = "admin@clevertech.biz";
+		$adminUser->password = "clevertech";
+
+		$adminUser->save();
+
+	}
+
+	public function down()
+	{
+		$this->dropTable('user');
+	}
+}
