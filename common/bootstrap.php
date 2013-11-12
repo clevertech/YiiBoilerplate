416efa84733dc189b08b70662a33f718b5de237e
Saphronov Mark <hijarian@gmail.com>
2013-11-12 12:21:43 +0400

    Converted comments to shell-format.

--- common/bootstrap.php
+++ common/bootstrap.php
@@ -1,13 +1,12 @@
 <?php
-/**
- * This is the global bootstrap file containing code which should be run for *every* entry point.
- */
+# This is the global bootstrap file containing code which should be run for *every* entry point.
 
-// Path to the real root of project
+
+# Path to the real root of project
 define('ROOT_DIR', realpath(__DIR__ . '/..'));
 
-// Depending on various triggers you can enable debug mode for the project here.
-// You can control debug mode with additional Yii console command named `debugmode`: `./yiic debugmode on`
+# Depending on various triggers you can enable debug mode for the project here.
+# You can control debug mode with additional Yii console command named `debugmode`: `./yiic debugmode on`
 if (file_exists(ROOT_DIR . '/debugmodeon') // enabled via the flag file
     or !empty($_SERVER['YII_DEBUG']) // enabled via the $_SERVER var
     or getenv('YII_DEBUG') // enabled via the environment var
@@ -15,37 +14,37 @@ if (file_exists(ROOT_DIR . '/debugmodeon') // enabled via the flag file
     require_once __DIR__ . '/debugmode.php';
 }
 
-// If we have autoloader from Composer, use it.
+# If we have autoloader from Composer, use it.
 if (file_exists(ROOT_DIR . '/vendor/autoload.php'))
     require ROOT_DIR . '/vendor/autoload.php';
 
-// NOTE that you must declare `YII_DEBUG` and `YII_TRACE_LEVEL`
-// BEFORE loading the framework or it will have no effect on Yii!
+# NOTE that you must declare `YII_DEBUG` and `YII_TRACE_LEVEL`
+# BEFORE loading the framework or it will have no effect on Yii!
 
-// Launching the Yii framework.
+# Launching the Yii framework.
 require_once ROOT_DIR . '/vendor/yiisoft/yii/framework/YiiBase.php';
-// Include our own Yii singleton definition
+# Include our own Yii singleton definition
 require_once(ROOT_DIR.'/common/components/Yii.php');
-// Include our own base WebApplication class
+# Include our own base WebApplication class
 require_once(ROOT_DIR.'/common/components/WebApplication.php');
-// Include our own helper methods library
+# Include our own helper methods library
 require_once(ROOT_DIR.'/common/lib/global.php');
 
-// Yes, we "want to append additional autoloaders to the default Yii autoloader".
+# Yes, we "want to append additional autoloaders to the default Yii autoloader".
 YiiBase::$enableIncludePath = false;
 
-// Some global aliases
+# Some global aliases
 Yii::setPathOfAlias('root', ROOT_DIR);
 Yii::setPathOfAlias('common', ROOT_DIR . '/common');
 Yii::setPathOfAlias('vendor', ROOT_DIR . '/vendor');
 
-// Global timezone setting
+# Global timezone setting
 date_default_timezone_set('UTC');
 
-// Set the internal character encoding
+# Set the internal character encoding
 mb_internal_encoding("UTF-8");
 
-// We're in XXI century, muthafucka!
+# We're in XXI century, so let's use modern locale already
 setlocale(
     LC_CTYPE,
     'C.UTF-8', // libc >= 2.13
@@ -54,5 +53,5 @@ setlocale(
     'en_US.utf8' // different spelling for fallback
 );
 
-// PWD will be the root dir of the project
+# PWD will be the root dir of the project
 chdir(ROOT_DIR);
