d1af7b508bc2eb028f14f3e857f29768b07e75e0
Saphronov Mark <hijarian@gmail.com>
2013-11-13 12:55:45 +0400

    Rearranged metatags in frontend layout.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -11,12 +11,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-	<!-- Use the .htaccess and remove these lines to avoid edge case issues. More info: h5bp.com/b/378 -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
