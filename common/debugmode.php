60e444da79671684bbf2ce2dcf5686038c5e58d3
Saphronov Mark <hijarian@gmail.com>
2013-11-13 13:04:06 +0400

    Changed the comments to shell-style in debugmode.php

--- common/debugmode.php
+++ common/debugmode.php
@@ -6,21 +6,21 @@
  * If such a file exists (contents are not important) then global bootstrap will execute the following code.
 */
 
-// Our custom marker for debug mode
+# Our custom marker for debug mode
 define('DEBUG_MODE', true);
 
-// Please be sure that both of the following constants are NOT defined elsewhere.
-// `defined` checks were removed intentionally.
-
-// Yii's marker to note that we need Yii::trace() enabled;
+# Please be sure that both of the following constants are NOT defined elsewhere.
+# `defined` checks were removed intentionally.
+#
+# Yii's marker to note that we need Yii::trace() enabled;
 define('YII_DEBUG', true);
-// Stacktraces 3 level deep
+# Stacktraces 3 level deep
 define('YII_TRACE_LEVEL', 3);
 
-// NOTE: any Yii tracing to the files or browser window will happen only if you setup the CLogRoutes in the config!
-// Until then, YII_DEBUG and YII_TRACE_LEVEL constants are meaningless.
-
-// In debug mode we really want to print errors to client
+# NOTE: any Yii tracing to the files or browser window will happen only if you setup the CLogRoutes in the config!
+# Until then, YII_DEBUG and YII_TRACE_LEVEL constants are meaningless.
+#
+# In debug mode we really want to print errors to client
 ini_set('display_errors', true);
-// Report absolutely everything
+# Report absolutely everything
 error_reporting(-1);
