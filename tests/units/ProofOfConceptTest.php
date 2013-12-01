a9fdc9b18596432db73790e04f2302b9801fde9f
Saphronov Mark <hijarian@gmail.com>
2013-12-01 14:17:02 +0400

    Added some proof-of-concept changes in tests directories.

---
+++ tests/units/ProofOfConceptTest.php
@@ -0,0 +1,12 @@
+<?php
+/** hijarian 01.12.13 12:22 */
+
+class ProofOfConceptTest extends PHPUnit_Framework_TestCase
+{
+	/** @test */
+	public function CanDoNothing()
+	{
+		// do nothing
+	}
+}
+ 
