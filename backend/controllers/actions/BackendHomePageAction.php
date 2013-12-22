d0e2e1f8cfc46a6420b777bf4e61d2816b2e1852
Mark Safronov <hijarian@gmail.com>
2013-12-22 15:02:56 +0400

    Cleaned up the BackendHomePageAction class.

--- backend/controllers/actions/BackendHomePageAction.php
+++ backend/controllers/actions/BackendHomePageAction.php
@@ -1,8 +1,19 @@
 <?php
-/** hijarian @ 12.11.13 13:50 */
-
+/**
+ * Controller action representing the act of home page rendering on backend.
+ *
+ * Everything which should be done when user opens the backend landing page is here.
+ *
+ * @author Mark Safronov marks@clevertech.biz
+ * @copyright 2013 Clevertech
+ * @license BSD
+ * @package YiiBoilerplate\Backend
+ */
 class BackendHomePageAction extends CAction
 {
+    /**
+     * We render the homepage as a controller action here.
+     */
     public function run()
     {
         $this->controller->render('index');
