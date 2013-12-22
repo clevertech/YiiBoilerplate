36b338e59481415fed861e9e9dedce3789354021
Mark Safronov <hijarian@gmail.com>
2013-12-22 10:41:31 +0400

    Converted the comments in backend and frontend entry points to shell-style.

--- backend/www/index.php
+++ backend/www/index.php
@@ -1,29 +1,26 @@
 <?php
 /**
- * backend/www/index.php
- *
- * Entry point for backend
+ * Entry point script for backend.
  *
  * @author: antonio ramirez <antonio@clevertech.biz>
- * Date: 7/22/12
- * Time: 11:13 AM
- *
  * @author: mark safronov <hijarian@gmail.com>
  */
 
-// Loading project default init code for all entry points.
+# Loading project default init code for all entry points.
 require __DIR__.'/../../common/bootstrap.php';
 
-// Setting up the frontend-specific aliases
+# Setting up the frontend-specific aliases
 Yii::setPathOfAlias('backend', ROOT_DIR .'/backend');
 Yii::setPathOfAlias('www', ROOT_DIR . '/backend/www');
 
-// As we are using BootstrapFilter to include Booster, we have to define 'bootstrap' alias ourselves
+# As we are using BootstrapFilter to include Booster, we have to define 'bootstrap' alias ourselves
+# Note that we are binding to Composer-installed version of YiiBooster
 Yii::setPathOfAlias('bootstrap', ROOT_DIR . '/vendor/clevertech/yii-booster/src');
 
-// We use our custom-made WebApplication component as base class for backend app.
+# We use our custom-made WebApplication component as base class for backend app.
 require_once ROOT_DIR.'/backend/components/BackendWebApplication.php';
 
+# For obvious reasons, backend entry point is constructed of specialised WebApplication and config
 Yii::createApplication(
     'BackendWebApplication',
     ROOT_DIR.'/backend/config/main.php'
--- frontend/www/index.php
+++ frontend/www/index.php
@@ -1,26 +1,22 @@
 <?php
 /**
- * /frontend/www/index.php
- *
  * Entry point for the frontend.
  *
  * @author: antonio ramirez <antonio@clevertech.biz>
- * Date: 7/22/12
- * Time: 11:13 AM
- *
  * @author: mark safronov <hijarian@gmail.com>
  */
 
-// Loading project default init code for all entry points.
+# Loading project default init code for all entry points.
 require __DIR__.'/../../common/bootstrap.php';
 
-// Setting up the frontend-specific aliases
+# Setting up the frontend-specific aliases
 Yii::setPathOfAlias('frontend', ROOT_DIR .'/frontend');
 Yii::setPathOfAlias('www', ROOT_DIR . '/frontend/www');
 
-// We use our custom-made WebApplication component as base class for frontend app.
+# We use our custom-made WebApplication component as base class for frontend app.
 require_once ROOT_DIR.'/frontend/components/FrontendWebApplication.php';
 
+# For obvious reasons, backend entry point is constructed of specialised WebApplication and config
 Yii::createApplication(
 	'FrontendWebApplication',
 	ROOT_DIR.'/frontend/config/main.php'
