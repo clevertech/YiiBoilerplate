ac68e999029c336a62d578fe856c49ef94a9b6dd
Mark Safronov <hijarian@gmail.com>
2013-12-22 14:31:43 +0400

    Renamed the UserIdentity to AdminIdentity and cleaned it up.

---
+++ backend/components/AdminIdentity.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Authentication information about the admin user registered in our database.
+ *
+ * This is the override of basic CUserIdentity which authenticates users using User model,
+ * i. e. it assumes that you will have administrator users and normal users in the same database table.
+ *
+ * @author Mark Safronov marks@clevertech.biz
+ * @copyright 2013 Clevertech
+ * @license BSD
+ * @package YiiBoilerplate\Backend
+ */
+class AdminIdentity extends CUserIdentity
+{
+	/** @var integer ID of logged user */
+	private $id;
+
+	/**
+     * Getter method for `id` property.
+     *
+	 * @return integer Internal database ID of the user, null if not set
+	 */
+	public function getId()
+    {
+		return $this->id;
+	}
+
+	/**
+	 * Given that the `username` and `password` properties of this object is set,
+     * tries to authenticate the user using the User model.
+     *
+     * On successful authentication fills in its `id` and `username` properties from the User model.
+     *
+	 * @return boolean Whether we have such a user in database and password is correct or not.
+	 */
+	public function authenticate()
+    {
+        $user = $this->findUser();
+        if (!$user)
+            return $this->failureBecauseUserNotFound();
+
+        if (!$user->verifyPassword($this->password))
+            return $this->failureBecausePasswordInvalid();
+
+        $this->makeAuthenticated($user);
+
+		return $this->isAuthenticated;
+	}
+
+
+    /** @return User */
+    private function findUser()
+    {
+        return User::model()->find(
+            [
+                'condition' => 'username=:username',
+                'params' => [':username' => $this->username]
+            ]
+        );
+    }
+
+    private function failureBecauseUserNotFound()
+    {
+        $this->errorCode = self::ERROR_USERNAME_INVALID;
+        return false;
+    }
+
+    private function failureBecausePasswordInvalid()
+    {
+        $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        return false;
+    }
+
+    /** @param User $user */
+    private function makeAuthenticated($user)
+    {
+        $user->regenerateValidationKey();
+        $this->id = $user->id;
+        $this->username = $user->username;
+        $this->setState('vkey', $user->validation_key);
+        $this->errorCode = self::ERROR_NONE;
+    }
+}
--- backend/models/LoginForm.php
+++ backend/models/LoginForm.php
@@ -51,7 +51,7 @@ class LoginForm extends CFormModel {
 	 */
 	public function authenticate($attribute, $params) {
 		if (!$this->hasErrors()) {
-			$this->_identity = new UserIdentity($this->username, $this->password);
+			$this->_identity = new AdminIdentity($this->username, $this->password);
 			if (!$this->_identity->authenticate()) {
 				if (($user = $this->user) !== null && $user->login_attempts < 100)
 					$user->saveAttributes(array('login_attempts' => $user->login_attempts + 1));
@@ -77,10 +77,10 @@ class LoginForm extends CFormModel {
 	 */
 	public function login() {
 		if ($this->_identity === null) {
-			$this->_identity = new UserIdentity($this->username, $this->password);
+			$this->_identity = new AdminIdentity($this->username, $this->password);
 			$this->_identity->authenticate();
 		}
-		if ($this->_identity->errorCode === UserIdentity::ERROR_NONE) {
+		if ($this->_identity->errorCode === AdminIdentity::ERROR_NONE) {
 			$duration = $this->rememberMe ? 3600 * 24 * 30 : 0; // 30 days
 			Yii::app()->user->login($this->_identity, $duration);
 			return true;
