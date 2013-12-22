e15f5ab37ce13f1a648bd098094cdc2302e5f83d
Mark Safronov <hijarian@gmail.com>
2013-12-22 18:13:51 +0400

    Returned missing spaces in function calls in views for User CRUD.

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -20,87 +20,87 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'username'); ?>
+		<?php echo $form->labelEx($model, 'username'); ?>
+		<?php echo $form->textField($model, 'username', array('size'=>45, 'maxlength'=>45)); ?>
+		<?php echo $form->error($model, 'username'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'password'); ?>
+		<?php echo $form->labelEx($model, 'password'); ?>
+		<?php echo $form->passwordField($model, 'password', array('size'=>60, 'maxlength'=>255)); ?>
+		<?php echo $form->error($model, 'password'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'salt'); ?>
-		<?php echo $form->textField($model,'salt',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'salt'); ?>
+		<?php echo $form->labelEx($model, 'salt'); ?>
+		<?php echo $form->textField($model, 'salt', array('size'=>60, 'maxlength'=>255)); ?>
+		<?php echo $form->error($model, 'salt'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password_strategy'); ?>
-		<?php echo $form->textField($model,'password_strategy',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'password_strategy'); ?>
+		<?php echo $form->labelEx($model, 'password_strategy'); ?>
+		<?php echo $form->textField($model, 'password_strategy', array('size'=>50, 'maxlength'=>50)); ?>
+		<?php echo $form->error($model, 'password_strategy'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'requires_new_password'); ?>
-		<?php echo $form->textField($model,'requires_new_password'); ?>
-		<?php echo $form->error($model,'requires_new_password'); ?>
+		<?php echo $form->labelEx($model, 'requires_new_password'); ?>
+		<?php echo $form->textField($model, 'requires_new_password'); ?>
+		<?php echo $form->error($model, 'requires_new_password'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'email'); ?>
+		<?php echo $form->labelEx($model, 'email'); ?>
+		<?php echo $form->textField($model, 'email', array('size'=>60, 'maxlength'=>255)); ?>
+		<?php echo $form->error($model, 'email'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'login_attempts'); ?>
-		<?php echo $form->textField($model,'login_attempts'); ?>
-		<?php echo $form->error($model,'login_attempts'); ?>
+		<?php echo $form->labelEx($model, 'login_attempts'); ?>
+		<?php echo $form->textField($model, 'login_attempts'); ?>
+		<?php echo $form->error($model, 'login_attempts'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'login_time'); ?>
-		<?php echo $form->textField($model,'login_time'); ?>
-		<?php echo $form->error($model,'login_time'); ?>
+		<?php echo $form->labelEx($model, 'login_time'); ?>
+		<?php echo $form->textField($model, 'login_time'); ?>
+		<?php echo $form->error($model, 'login_time'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'login_ip'); ?>
-		<?php echo $form->textField($model,'login_ip',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'login_ip'); ?>
+		<?php echo $form->labelEx($model, 'login_ip'); ?>
+		<?php echo $form->textField($model, 'login_ip', array('size'=>32, 'maxlength'=>32)); ?>
+		<?php echo $form->error($model, 'login_ip'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'validation_key'); ?>
-		<?php echo $form->textField($model,'validation_key',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'validation_key'); ?>
+		<?php echo $form->labelEx($model, 'validation_key'); ?>
+		<?php echo $form->textField($model, 'validation_key', array('size'=>60, 'maxlength'=>255)); ?>
+		<?php echo $form->error($model, 'validation_key'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'create_id'); ?>
-		<?php echo $form->textField($model,'create_id'); ?>
-		<?php echo $form->error($model,'create_id'); ?>
+		<?php echo $form->labelEx($model, 'create_id'); ?>
+		<?php echo $form->textField($model, 'create_id'); ?>
+		<?php echo $form->error($model, 'create_id'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'create_time'); ?>
-		<?php echo $form->textField($model,'create_time'); ?>
-		<?php echo $form->error($model,'create_time'); ?>
+		<?php echo $form->labelEx($model, 'create_time'); ?>
+		<?php echo $form->textField($model, 'create_time'); ?>
+		<?php echo $form->error($model, 'create_time'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'update_id'); ?>
-		<?php echo $form->textField($model,'update_id'); ?>
-		<?php echo $form->error($model,'update_id'); ?>
+		<?php echo $form->labelEx($model, 'update_id'); ?>
+		<?php echo $form->textField($model, 'update_id'); ?>
+		<?php echo $form->error($model, 'update_id'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'update_time'); ?>
-		<?php echo $form->textField($model,'update_time'); ?>
-		<?php echo $form->error($model,'update_time'); ?>
+		<?php echo $form->labelEx($model, 'update_time'); ?>
+		<?php echo $form->textField($model, 'update_time'); ?>
+		<?php echo $form->error($model, 'update_time'); ?>
 	</div>
 
 	<div class="row buttons">
--- backend/views/user/admin.php
+++ backend/views/user/admin.php
@@ -33,11 +33,9 @@ You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&g
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Advanced Search', '#', array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
+<?php $this->renderPartial('_search', compact('model')); ?>
 </div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
