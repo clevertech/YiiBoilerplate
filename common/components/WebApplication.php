bb0574c27b7d4df2f418693162d42f7229a22940
Mark Safronov <hijarian@gmail.com>
2014-01-01 14:13:02 +0400

    A bit of cleanup in WebApplication class.

--- common/components/WebApplication.php
+++ common/components/WebApplication.php
@@ -1,42 +1,26 @@
 <?php
 /**
- * WebApplication.php
+ * Base class for our web application.
  *
- * From Alex Makarov boilerplate
- *
- * @author: antonio ramirez <antonio@clevertech.biz>
- * Date: 7/23/12
- * Time: 12:44 AM
+ * Here will be the tweaks affecting the behavior of all entry points.
  */
-class WebApplication extends CWebApplication {
-
+class WebApplication extends CWebApplication
+{
 	/**
-	 * This function is here because we aren't creating a locale file for every client.
-	 * Thus we provide a fallback to "en".
+     * Workaround to fallback to `en` locale even if something unknown to us was requested.
+     *
+     * @param string $localeID
+     * @return CLocale
 	 */
-	public function getLocale($localeID = null) {
-		try {
+	public function getLocale($localeID = null)
+    {
+		try
+        {
 			return parent::getLocale($localeID);
-		} catch (Exception $e) {
+		}
+        catch (Exception $e)
+        {
 			return CLocale::getInstance('en');
 		}
 	}
-
-	/**
-	 * We were getting tons of errors in the logs from OPTIONS requests for the URI "*"
-	 * As it turns out, the requests were from localhost (::1) and are apparently a way
-	 * that Apache polls its processes to see if they're alive. This function causes
-	 * Yii to respond without logging errors.
-	 */
-	public function runController($route) {
-		try {
-			parent::runController($route);
-		} catch (CHttpException $e) {
-			if (@$_SERVER['REQUEST_METHOD'] == 'OPTIONS' && @$_SERVER['REQUEST_URI'] == '*') {
-				Yii::app()->end('Hello, friend!');
-			} else
-				throw $e;
-		}
-	}
-
 }
