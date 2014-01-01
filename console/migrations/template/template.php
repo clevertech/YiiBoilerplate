2719b07aa5bce30cb832095c47533d881bfaf23f
Mark Safronov <hijarian@gmail.com>
2014-01-01 15:47:25 +0400

    Added missing migrations template.

---
+++ console/migrations/template/template.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TODO: Migration description
+ *
+ * @package migrations
+ */
+class {ClassName} extends CDbMigration
+{
+
+	public function up() 
+	{
+
+		// TODO: Migration actions
+
+	}
+
+	public function down() 
+	{
+
+		// TODO: Migration rollback actions
+
+		echo "{ClassName} does not support migration down.\\n";
+		return false;
+	}
+
+}
+
