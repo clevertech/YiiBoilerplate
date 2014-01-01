d601a05d7864e2d8f3d8bd074fb1654304077f3c
Mark Safronov <hijarian@gmail.com>
2014-01-01 16:16:32 +0400

    Added predefined parameter for Google Analytics ID inside frontend/prod config as an example of usage.

--- frontend/config/environments/prod.php
+++ frontend/config/environments/prod.php
@@ -3,6 +3,9 @@
  * Specific config overrides for frontend entry point at production server.
  */
 return [
-    // Normally you don't have anything to specify for frontend entry point at production server.
+    'params' => [
+        // Provide your real GA ID so the relevant code will automatically be inserted in layout.
+        'google.analytics.id' => 'XXXX-XXXX-XXXX'
+    ]
 ];
 
