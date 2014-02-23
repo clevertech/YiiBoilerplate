a3b9cb9dcfc099388b7aa572e2be665d4acd0807
Mark Safronov <hijarian@gmail.com>
2014-02-23 04:25:19 +0000

    Added some guards to the Composer installation procedure at Vagrant provision script.

--- carcass/vagrant/setup-app.sh
+++ carcass/vagrant/setup-app.sh
@@ -4,7 +4,8 @@
 cd /vagrant
 
 # Firing up the Composer to install thirdparty libraries
-php composer.phar install
+php composer.phar self-update
+php composer.phar install --prefer-dist
 
 # Note that all binaries will be installed into `bin/` under ROOT_DIR
 # including phpunit, behat, phing and yiic
